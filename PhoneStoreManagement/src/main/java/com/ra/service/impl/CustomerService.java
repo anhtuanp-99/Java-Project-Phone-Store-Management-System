8a2ab5846f7213c0e6f06573befcb56446eda31c
anhtuanp-99 <anhtuanhl2004@gmail.com>
2026-06-28 00:55:29 +0700

    update

--- PhoneStoreManagement/src/main/java/com/ra/service/impl/CustomerService.java
+++ PhoneStoreManagement/src/main/java/com/ra/service/impl/CustomerService.java
@@ -1,42 +1,72 @@
 package com.ra.service.impl;
 
+import com.ra.config.DBConnection;
 import com.ra.model.Customer;
+import com.ra.repository.ICustomerRepository;
+import com.ra.repository.impl.CustomerRepository;
 import com.ra.service.ICustomerService;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.util.ArrayList;
 import java.util.List;
 
 public class CustomerService implements ICustomerService {
+
+    private final ICustomerRepository customerRepo;
+
+    public CustomerService(){
+        this.customerRepo = new CustomerRepository();
+    }
 
 
 
     @Override
     public List<Customer> findAll() {
-
-        return List.of();
+        return customerRepo.findAll();
     }
 
     @Override
     public Customer findById(int id) {
-        return null;
+        Customer customer = customerRepo.findById(id);
+        if (customer == null) {
+            throw new RuntimeException("Không tìm thấy khách hàng với ID: " + id);
+        }
+        return customer;
     }
 
     @Override
     public Customer findByEmail(String email) {
-        return null;
+        Customer customer = customerRepo.findByEmail(email);
+        if (customer == null) {
+            throw new RuntimeException("Không tìm thấy khách hàng với email " + email);
+        }
+        return customerRepo.findByEmail(email);
     }
 
     @Override
     public boolean save(Customer customer) {
-        return false;
+        if (customer.getName() == null || customer.getName().isBlank()){
+            throw new RuntimeException("Tên khách hàng không được để trống!");
+        }
+        if (customer.getEmail() == null || customer.getEmail().isBlank()){
+            throw new RuntimeException("Email khách hàng không được để trống!");
+        }
+        if (customerRepo.findByEmail(customer.getEmail()) != null) {
+            throw new RuntimeException("Email đã tồn tại trong hệ thống");
+        }
+        return customerRepo.save(customer);
     }
 
     @Override
     public boolean update(Customer customer) {
-        return false;
+        findById(customer.getId()); // kiểm tra khách hàng tồn tại trước khi update
+        return customerRepo.update(customer);
     }
 
     @Override
     public boolean delete(int id) {
-        return false;
+        findById(id);
+        return customerRepo.delete(id);
     }
 }
